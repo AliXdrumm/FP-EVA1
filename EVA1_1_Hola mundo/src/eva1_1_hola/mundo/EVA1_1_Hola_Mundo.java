a3dbd3313b3d5a5c3cbd6b3e6b89672a0ed6c3b1
AliXdrumm <l25550288@chihuahua2.tecnm.mx>
2025-08-26 16:34:08 -0600

    Add files via upload

---
+++ EVA1_1_Hola mundo/src/eva1_1_hola/mundo/EVA1_1_Hola_Mundo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_1_hola.mundo;
+
+/**
+ *
+ * @author licce
+ */
+public class EVA1_1_HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!!");
+        System.out.println("FUNDAMENTOS DE PROGRAMACIÓN");
+       
+    }
+    
+}
