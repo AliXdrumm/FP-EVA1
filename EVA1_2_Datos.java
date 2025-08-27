442008b0c45f8e17f1f5d63df5e0005ef461f3ae
AliXdrumm <l25550288@chihuahua2.tecnm.mx>
2025-08-27 09:49:55 -0600

    Add files via upload

---
+++ EVA1_2_Datos.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_datos;
+
+/**
+ *
+ * @author licce
+ */
+public class EVA1_2_Datos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Alan Armando Rodríguez Jiménez");
+        //NUMEROS ENTEROS
+        System.out.println(1000);
+        //NUMEROS DECIMALES
+        System.out.println(53.5);
+        //VALORES LOGICOS (FALSO Y VERDADERO)
+        //true false
+        System.out.println(true);
+        //CARACTERES INDIVIDUALES
+        //COSAS DISTINTAS
+        System.out.println('c');
+        System.out.println("c");
+    }
+    
+}
