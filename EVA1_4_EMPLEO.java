6d0e1b876bbdd4c499482b121db151ceb7ee7da5
AliXdrumm <l25550288@chihuahua2.tecnm.mx>
2025-08-28 16:12:03 -0600

    Add files via upload

---
+++ EVA1_4_EMPLEO.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package EVA1_4_EJERCICIO;
+
+/**
+ *
+ * @author licce
+ */
+public class EVA1_4_EMPLEO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Empleo: ");
+        System.out.println("Desarrollador de aplicaciones");
+        System.out.print("Salario: ");
+        System.out.println("14,000 Mensuales");
+        System.out.print("Horas a la semana: ");
+        System.out.println("35 Horas");
+        System.out.print("Necesita ingles?: ");
+        System.out.println(true);
+    }
+    
+}
