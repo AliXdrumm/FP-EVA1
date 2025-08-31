c3f9e65c30f13da451f98d5f3fd592345248affb
AliXdrumm <l25550288@chihuahua2.tecnm.mx>
2025-08-31 13:24:55 -0600

    Add files via upload

---
+++ EVA1_5_VARIABLES1.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_5_variables1;
+
+/**
+ *
+ * @author licce
+ */
+public class EVA1_5_VARIABLES1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre;
+        int edad;
+        double salario;
+        //INICIALIZACION:
+        //IDENTIFICADOR = valor;
+        //= --> operador de asignacion
+        nombre = "Alan Armando Rodriguez Jimenez";
+        edad = 18;
+        salario = 10020.12;
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);
+    }
+    
+}
